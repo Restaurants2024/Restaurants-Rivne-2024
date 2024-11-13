e82dc6d2cc86be0eebf8b6b51664407ea108992f
kappivna <yarmoliuk_ak23@nuwm.edu.ua>
2024-11-13 17:37:02 +0200

    Intergated in ASP NET MVC

---
+++ UI/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ContactController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
